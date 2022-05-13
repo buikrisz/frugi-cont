56c58e0810a079a6c09b3aea36a09215b25eddfc
Krisztian Bui <buikrisz00@gmail.com>
2022-05-13 13:53:56 +0200

    ozone_temp added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PestControl, Ozone } from './containers';
+import { PestControl, Ozone, Ozone_temp } from './containers';
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <PestControl /> }/>
-                <Route path="/ozone" element={ <Ozone /> }/>
+                <Route path="/ozone" element={ <Ozone_temp /> }/>
             </Routes>
         </BrowserRouter>
     );
